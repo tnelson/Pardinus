035a625d53052f37039bc009edcae17fac5ea2e4
nmacedo <nfmmacedo@di.uminho.pt>
2017-10-20 18:43:50 +0100

    generalized formula slicer to consider set of variables

--- src/main/java/kodkod/engine/decomp/DecompFormulaSlicer.java
+++ src/main/java/kodkod/engine/decomp/DecompFormulaSlicer.java
@@ -22,41 +22,109 @@
  */
 package kodkod.engine.decomp;
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import kodkod.ast.BinaryFormula;
 import kodkod.ast.Formula;
 import kodkod.ast.NaryFormula;
+import kodkod.ast.Relation;
 import kodkod.ast.operator.FormulaOperator;
-import kodkod.engine.config.DecomposedOptions;
+import kodkod.engine.fol2sat.FormulaFlattener;
+import kodkod.engine.fol2sat.RelationCollector;
+import kodkod.instance.PardinusBounds;
+import kodkod.util.nodes.AnnotatedNode;
 
 /**
+ * Slices a first-order logic formula into a pair of conjuncts given a set of
+ * variables as the slicing criteria, intended to be used during decomposed
+ * model finding. Uses a {@link FormulaFlattener formula flattener} to reduce to
+ * NNF and flatten conjuncts, which are then filtered according to the set of
+ * variables.
+ * 
+ * The {@link PardinusBounds bounds} with decomposed information. If the problem
+ * is not integrated (still at the first stage), then only set of partial
+ * variables occurs in the bounds. If integrated, all variables occur in the
+ * bounds, and only those still not constant are considered for the second
+ * stage. If the process is at the second stage and every variable is already
+ * constant returns the whole formula (probably a case of trivial solution
+ * iteration).
+ * 
  * @author Nuno Macedo // [HASLab] decomposed model finding
  */
 public class DecompFormulaSlicer {
 
-	public final Formula f1;
-	public final Formula f2;
-	
-	public DecompFormulaSlicer(Formula formula, DecomposedOptions<?> options) {
-		// TODO: allow different slicing modes on options
-		if (formula instanceof BinaryFormula && ((BinaryFormula) formula).op() == FormulaOperator.AND) {
-			f1 = ((BinaryFormula) formula).left();
-			f2 = ((BinaryFormula) formula).right();
-		} else if (formula instanceof NaryFormula && ((NaryFormula) formula).op() == FormulaOperator.AND) {
-			Iterator<Formula> it = ((NaryFormula) formula).iterator();
+	/**
+	 * Slices the conjuncts of a formula into two, depending on the variables
+	 * selected for the partial problem, as defined in the bounds of the
+	 * problem. Considers whether the bounds are already integrated to retrieve
+	 * the relevant variables. 
+	 *
+	 * @param formula
+	 *            the formula to be sliced.
+	 * @param bounds
+	 *            the bounds containing information regarding the selected
+	 *            variables.
+	 * @return two conjuncts of the formula according to the selected variables.
+	 */
+	public static Entry<Formula, Formula> slice(Formula formula,
+			PardinusBounds bounds) {
+		Set<Relation> partials = bounds.relations();
+		partials.addAll(bounds.relationsSymb());
+		if (bounds.integrated) {
+			Set<Relation> aux = new HashSet<Relation>();
+			for (Relation r : partials)
+				if (bounds.lowerBound(r).size() == bounds.upperBound(r).size())
+					aux.add(r);
+			// iterating trivial integrated solutions
+			// TODO: there is probably a better way to handle this
+			if (partials.equals(aux))
+				return new SimpleEntry<Formula, Formula>(Formula.TRUE, formula);
+			partials = aux;
+		}
+		// converts to NNF and flattens the formula
+		AnnotatedNode<Formula> flat = FormulaFlattener.flatten(
+				AnnotatedNode.annotateRoots(formula), false);
+		Formula f1 = flat.node();
+		Formula f2 = Formula.TRUE;	
+		RelationCollector col = new RelationCollector(flat.sharedNodes());
+		// select the appropriate conjuncts
+		if (f1 instanceof BinaryFormula
+				&& ((BinaryFormula) f1).op() == FormulaOperator.AND) {
+			Set<Relation> rsl = ((BinaryFormula) f1).left().accept(col);
+			Set<Relation> rsr = ((BinaryFormula) f1).right().accept(col);
+			if (partials.containsAll(rsl)) {
+				if (!partials.containsAll(rsr)) {
+					f2 = ((BinaryFormula) f1).right();
+					f1 = ((BinaryFormula) f1).left();
+				}
+			} else {
+				if (partials.containsAll(rsr)) {
+					f2 = ((BinaryFormula) f1).left();
+					f1 = ((BinaryFormula) f1).right();
+				} else {
+					f2 = f1;
+					f1 = Formula.TRUE;
+				}
+			}
+		} else if (f1 instanceof NaryFormula
+				&& ((NaryFormula) f1).op() == FormulaOperator.AND) {
+			Iterator<Formula> it = ((NaryFormula) f1).iterator();
 			f1 = it.next();
 			Formula aux = Formula.TRUE;
-			while (it.hasNext())
-				aux = aux.and(it.next());
-			f2 = aux;
-		} else if (formula == Formula.FALSE){
-			f1 = formula;
-			f2 = Formula.FALSE;
-		} else {
-			f1 = formula;
-			f2 = formula;
+			while (it.hasNext()) {
+				Formula f = it.next();
+				Set<Relation> rs = f.accept(col);
+				if (partials.containsAll(rs))
+					aux = aux.and(f);
+				else
+					f2 = f2.and(f);
+			}
+			f1 = aux;
 		}
-
+		return new SimpleEntry<Formula, Formula>(f1, f2);
 	}
 }
