df84a56aae2e845e953371bbf1f20189d43bd068
nmacedo <nfmmacedo@di.uminho.pt>
2018-05-14 11:11:20 +0100

    may expand with symbolic is resolved

--- src/main/java/kodkod/engine/ltl2fol/TemporalBoundsExpander.java
+++ src/main/java/kodkod/engine/ltl2fol/TemporalBoundsExpander.java
@@ -61,13 +61,11 @@ class TemporalBoundsExpander {
 	static private Universe createUniverse(PardinusBounds oldBounds, int numberOfTimes) {
 		List<Object> newAtoms = new ArrayList<Object>();
 		Iterator<Object> it = oldBounds.universe().iterator();
-		while (it.hasNext()) {
+		while (it.hasNext())
 			newAtoms.add(it.next());
-		}
 
-		for (int i = 0; i < numberOfTimes; i++) {
+		for (int i = 0; i < numberOfTimes; i++)
 			newAtoms.add(TemporalTranslator.STATEATOM + i);
-		}
 
 		return new Universe(newAtoms);
 	}
@@ -86,9 +84,8 @@ class TemporalBoundsExpander {
 		while (itr.hasNext()) {
 			Tuple t = itr.next();
 			List<Object> l = new ArrayList<Object>();
-			for (int i = 0; i < t.arity(); i++) {
+			for (int i = 0; i < t.arity(); i++)
 				l.add(t.atom(i));
-			}
 			tupleSet.add(newUniverse.factory().tuple(l));
 		}
 		return tupleSet;
@@ -98,41 +95,72 @@ class TemporalBoundsExpander {
 	/**
 	 * Expands the old bounds by converting the bounds over variable relations
 	 * into regular bounds with {@link TemporalTranslator#STATE temporal} atoms
-	 * appended. It also bounds the constant variables representing the trace.
+	 * appended. It also binds the constant variables representing the trace.
 	 * 
 	 * @param bounds
 	 *            the bounds with variable relations to be expanded.
-	 * @param traceLength
-	 *            The number of distinguished states in the trace.
-	 * @return
+	 * @param traceLen
+	 *            the number of distinguished states in the trace.
+	 * @return the expanded bounds
 	 */
-	static PardinusBounds expand(PardinusBounds bounds, int traceLength) {
-		Universe u = createUniverse(bounds, traceLength);
-		return expand(bounds, traceLength, u);
+	static PardinusBounds expand(PardinusBounds bounds, int traceLen) {
+		Universe u = createUniverse(bounds, traceLen);
+		return expand(bounds, traceLen, u);
 	}
 
-	private static PardinusBounds expand(PardinusBounds bounds, int traceLength, Universe u) {
+	/**
+	 * Actually expands temporal bounds into their static representation as regular
+	 * bounds with {@link TemporalTranslator#STATE temporal} atoms appended.
+	 * 
+	 * Bounds are assumed to already be expanded at this point. Also, trace bounds
+	 * with more than a state are not yet implemented.
+	 * 
+	 * TODO support trace bounds with multiple steps.
+	 * 
+	 * @param bounds
+	 *            the original temporal bounds
+	 * @param traceLen
+	 *            the number of distinguished states in the trace.
+	 * @param u
+	 *            the new universe
+	 * @return the expanded bounds with the new universe
+	 */
+	private static PardinusBounds expand(PardinusBounds bounds, int traceLen, Universe u) {
+		for (Relation r : bounds.relationsSymb())
+			if (bounds.upperBound(r) == null)
+				throw new UnsupportedOperationException("Expansion of symbolic bounds not yet supported.");
+		
+		if (bounds.relationsVars().size() > 1) 
+			throw new UnsupportedOperationException("Expansion of trace bounds not yet supported.");
+
 		PardinusBounds newBounds = new PardinusBounds(u);
 		TupleSet tupleSetTime = u.factory().range(
 				u.factory().tuple(new Object[] { TemporalTranslator.STATEATOM + "0" }),
-				u.factory().tuple(new Object[] { TemporalTranslator.STATEATOM + (traceLength - 1) }));
+				u.factory().tuple(new Object[] { TemporalTranslator.STATEATOM + (traceLen - 1) }));
 		bounds.first();
 		for (Relation r : bounds.relations()) {
 			if (r instanceof VarRelation) {
 				TupleSet tupleSetL = convert(bounds.lowerBound(r), u);
 				TupleSet tupleSetU = convert(bounds.upperBound(r), u);
 				newBounds.bound(((VarRelation) r).expanded, tupleSetL.product(tupleSetTime), tupleSetU.product(tupleSetTime));
+				if (bounds.target(r) != null) {
+					TupleSet tupleSetT = convert(bounds.target(r), u);
+					newBounds.setTarget(((VarRelation) r).expanded, tupleSetT.product(tupleSetTime));
+				}
+				if (bounds.weight(r) != null) 
+					newBounds.setWeight(((VarRelation) r).expanded, bounds.weight(r));
 			} else {
 				TupleSet tupleSetL = convert(bounds.lowerBound(r), u);
 				TupleSet tupleSetU = convert(bounds.upperBound(r), u);
-				newBounds.bound(r, tupleSetL, tupleSetU);			}
+				newBounds.bound(r, tupleSetL, tupleSetU);			
+				if (bounds.target(r) != null) {
+					TupleSet tupleSetT = convert(bounds.target(r), u);
+					newBounds.setTarget(r, tupleSetT.product(tupleSetTime));
+				}
+				if (bounds.weight(r) != null) 
+					newBounds.setWeight(r, bounds.weight(r));
+			}
 		}
-
-		if (bounds.relationsSymb().size() > 1) 
-			throw new UnsupportedOperationException("Expansion of symbolic bounds not yet supported.");
-
-		if (bounds.relationsVars().size() > 1) 
-			throw new UnsupportedOperationException("Expansion of trace bounds not yet supported.");
 
 		newBounds.bound(TemporalTranslator.STATE, tupleSetTime);
 		newBounds.bound(TemporalTranslator.FIRST, tupleSetTime);
@@ -142,7 +170,7 @@ class TemporalBoundsExpander {
 		newBounds.bound(TemporalTranslator.TRACE, tupleSetTime.product(tupleSetTime));
 		
 		if (bounds.amalgamated() != null) {
-			PardinusBounds newAmalg = expand(bounds.amalgamated(), traceLength, u);
+			PardinusBounds newAmalg = expand(bounds.amalgamated(), traceLen, u);
 			newBounds = new PardinusBounds(newBounds,newAmalg);
 		}
 		
